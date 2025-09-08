b1cfdbebb8ee6d6fc1b545be501a75c8fef26ba8
veins Code <44265346+saadidhia@users.noreply.github.com>
2025-09-08 17:15:59 +0100

    Update AuthApi.js

--- src/apis/AuthApi.js
+++ src/apis/AuthApi.js
@@ -13,7 +13,7 @@ export const authApi = {
 
 async function authenticate(username, password) {
 
-  return instance.post('auth/authenticate', { username, password }, {
+  return instance.post('api/auth/authenticate', { username, password }, {
     headers: {
       'Content-type': 'application/json'
     }
@@ -21,7 +21,7 @@ async function authenticate(username, password) {
 };
 
 function signup(user, admin) {
-  return instance.post("auth/signup", user, {
+  return instance.post("api/auth/signup", user, {
     headers: {
       Authorization: bearerAuth(admin),
       "Content-type": "application/json",
@@ -30,7 +30,7 @@ function signup(user, admin) {
 }
 
 function logout(user) {
-  return instance.post('auth/logout', null, {
+  return instance.post('api/auth/logout', null, {
     headers: {
       'Authorization': bearerAuth(user),
       'Content-type': 'application/json'
@@ -42,7 +42,7 @@ function logout(user) {
 // -- Axios
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API + "api/"
+  baseURL: process.env.REACT_APP_API
 })
 
 instance.interceptors.request.use(function (config) {
@@ -69,6 +69,7 @@ export function bearerAuth(user) {
   return `Bearer ${user.accessToken}`
 
 }
+
 
 
 
