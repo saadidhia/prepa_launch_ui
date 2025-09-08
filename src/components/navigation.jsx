72be2fab2ad70df4ac82ae36988f2786f2fdf91c
veins Code <44265346+saadidhia@users.noreply.github.com>
2025-09-08 08:55:58 +0100

    Update navigation.jsx

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -18,7 +18,7 @@ export const Navigation = (props) => {
             <span className="icon-bar"></span>{" "}
           </button>
           <a className="navbar-brand page-scroll" href="#page-top">
-            TIME4PREPA
+            TIME4PREPA0
           </a>{" "}
         </div>
 
